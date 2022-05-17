60a74a93bacace802e634cdb38539e1245d93869
skeleton2010 <inseong747@gmail.com>
2022-05-17 19:32:13 +0900

    http상태코드

--- app/src/models/UserStorge.js
+++ app/src/models/UserStorge.js
@@ -8,7 +8,7 @@ class UserStorge {
 
     static getUserInfo(id) {
         return new Promise((resolve, reject) => {
-            const query = "SELECT * FROM abc WHERE id = ?;";
+            const query = "SELECT * FROM users WHERE id = ?;";
             db.query(query, [id], (err, data) => {
                 if (err) reject(`${err}`);
                 else resolve(data[0]);
@@ -18,7 +18,7 @@ class UserStorge {
 
     static async save(usersInfo) {
         return new Promise((resolve, reject) => {
-            const query = "INSERT INTO abc(id, email, pw) VALUES(?,?,?);";
+            const query = "INSERT INTO users(id, email, pw) VALUES(?,?,?);";
             db.query(query, [usersInfo.id, usersInfo.email, usersInfo.pw], (err) => {
                 if (err) reject(`${err}`);
                 resolve({ successs: true });
